bfb01c7a49bb6b568c6d735e037a4a9a207fd533
MonarchDoge <kenzewee@gmail.com>
2016-12-25 17:06:27 +0800

    fuck lol

--- APPD/Services/UserServices.cs
+++ APPD/Services/UserServices.cs
@@ -4,8 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
-using Newtonsoft.Json;
 
 namespace APPD.Services
 {
@@ -13,30 +11,6 @@ namespace APPD.Services
     {
 
         public static List<User> readUsersFromDatabase()
-        {
-            // This is just a placeholder for the JSON database
-
-            List<User> Users = new List<User>();
-
-            string databaseURL = "pack://application:,,,/Assets/SimulatedServer/Database/Users.json";
-
-            Users.Add(new User(0, "PapaDanku", "pinkisawesome",
-                new List<AccountRentalData>(), new List<int>(), 1337, "yamete"));
-
-            Users.Add(new User(1, "theLegend27", "capitalism",
-                new List<AccountRentalData>(), new List<int>(), 9001, "Who is the legend 27?"));
-
-            Users.Add(new User(2, "XXXemokidXXX", "123456",
-                new List<AccountRentalData>(), new List<int>(), 60, "nigga please"));
-
-            // Make life easier (temporary account)
-            Users.Add(new User(3, "a", "a",
-                new List<AccountRentalData>(), new List<int>(), 60, "nigga please"));
-
-            return Users;
-        }
-
-        public static IEnumerable<User> LogIn(string username, string password)
         {
             string json = File.ReadAllText("Users.json");
             User u1 = JsonConvert.DeserializeObject<User>(json);
@@ -46,7 +20,15 @@ namespace APPD.Services
             {
                 yield return JsonConvert.DeserializeObject<User>(json);
             }
-            
+        }
+
+        public static User LogIn(string username, string password)
+        {
+            string json = FileStyleUriParser.ReadAllText("Users.json");
+            var playerList = json.DeserializeObject<List<Player>>(json);
+
+            File.WriteAllText("Users.json", json.SerializeObject(playerList));
+            return null;
         }
     }
 }
