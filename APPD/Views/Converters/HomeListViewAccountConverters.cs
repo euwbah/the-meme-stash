57c5407b5ac35768c43d7e5d9d1201e1dd723694
euwbah <euwbah@gmail.com>
2016-12-28 19:45:26 +0800

    awefawef

--- APPD/Views/Converters/HomeListViewAccountConverters.cs
+++ APPD/Views/Converters/HomeListViewAccountConverters.cs
@@ -16,24 +16,26 @@ namespace APPD.Views.Converters
     {
         public object Convert(object[] value, Type targetType, object parameter, CultureInfo culture)
         {
-            // The infolabel will only be visible if the account is owned, booked, fully booked, or active.
-            Account account = (Account)value[0];
-            User loggedOnUser = (User)value[1];
+            if (!value.Contains(DependencyProperty.UnsetValue))
+            {
+                // The infolabel will only be visible if the account is owned, booked, fully booked, or active.
+                Account account = (Account)value[0];
+                User loggedOnUser = (User)value[1];
 
-            // Fully booked
-            if (account.getListOfBookableDates().Count == 0)
-                return Visibility.Visible;
+                // Fully booked
+                if (account.getListOfBookableDates().Count == 0)
+                    return Visibility.Visible;
 
-            // Owned
-            else if (AccountServices.getAccountsOwnedBy(loggedOnUser)
-                        .Where(acc => acc.ID == account.ID).Count() != 0)
-                return Visibility.Visible;
+                // Owned
+                else if (AccountServices.getAccountsOwnedBy(loggedOnUser)
+                            .Where(acc => acc.ID == account.ID).Count() != 0)
+                    return Visibility.Visible;
 
-            // Booked / Active
-            else if (AccountServices.getAccountsRentedBy(loggedOnUser)
-                        .Where(acc => acc.ID == account.ID).Count() != 0)
-                return Visibility.Visible;
-
+                // Booked / Active
+                else if (AccountServices.getAccountsRentedBy(loggedOnUser)
+                            .Where(acc => acc.ID == account.ID).Count() != 0)
+                    return Visibility.Visible;
+            }
             return Visibility.Collapsed;
         }
 
@@ -46,29 +48,31 @@ namespace APPD.Views.Converters
     {
         public object Convert(object[] value, Type targetType, object parameter, CultureInfo culture)
         {
-            // The infolabel will only be visible if the account is owned, booked, fully booked, or active.
-            Account account = (Account)value[0];
-            User loggedOnUser = (User)value[1];
+            if (!value.Contains(DependencyProperty.UnsetValue))
+            {
+                // The infolabel will only be visible if the account is owned, booked, fully booked, or active.
+                Account account = (Account)value[0];
+                User loggedOnUser = (User)value[1];
 
-            // Fully booked
-            if (account.getListOfBookableDates().Count == 0)
-                return "SOLD OUT";
+                // Fully booked
+                if (account.getListOfBookableDates().Count == 0)
+                    return "SOLD OUT";
 
-            // Owned
-            else if (AccountServices.getAccountsOwnedBy(loggedOnUser)
-                        .Where(acc => acc.ID == account.ID).Count() != 0)
-                return "OWNED";
+                // Owned
+                else if (AccountServices.getAccountsOwnedBy(loggedOnUser)
+                            .Where(acc => acc.ID == account.ID).Count() != 0)
+                    return "OWNED";
 
-            // Active
-            else if (AccountServices.getAccountsRentedBy(loggedOnUser, true)
-                        .Where(acc => acc.ID == account.ID).Count() != 0)
-                return "ACTIVE";
+                // Active
+                else if (AccountServices.getAccountsRentedBy(loggedOnUser, true)
+                            .Where(acc => acc.ID == account.ID).Count() != 0)
+                    return "ACTIVE";
 
-            // Booked
-            else if (AccountServices.getAccountsRentedBy(loggedOnUser, false)
-                        .Where(acc => acc.ID == account.ID).Count() != 0)
-                return "BOOKED";
-
+                // Booked
+                else if (AccountServices.getAccountsRentedBy(loggedOnUser, false)
+                            .Where(acc => acc.ID == account.ID).Count() != 0)
+                    return "BOOKED";
+            }
             return "";
         }
 
@@ -81,29 +85,31 @@ namespace APPD.Views.Converters
     {
         public object Convert(object[] value, Type targetType, object parameter, CultureInfo culture)
         {
-            // The infolabel will only be visible if the account is owned, booked, fully booked, or active.
-            Account account = (Account)value[0];
-            User loggedOnUser = (User)value[1];
+            if (!value.Contains(DependencyProperty.UnsetValue))
+            {
+                // The infolabel will only be visible if the account is owned, booked, fully booked, or active.
+                Account account = (Account)value[0];
+                User loggedOnUser = (User)value[1];
 
-            // Fully booked
-            if (account.getListOfBookableDates().Count == 0)
-                return new SolidColorBrush(Color.FromArgb(0xff, 0xbb, 0x33, 0x11));
+                // Fully booked
+                if (account.getListOfBookableDates().Count == 0)
+                    return new SolidColorBrush(Color.FromArgb(0xff, 0xbb, 0x33, 0x11));
 
-            // Owned
-            else if (AccountServices.getAccountsOwnedBy(loggedOnUser)
-                        .Where(acc => acc.ID == account.ID).Count() != 0)
-                return new SolidColorBrush(Color.FromArgb(0xff, 0x22, 0xcc, 0x55));
+                // Owned
+                else if (AccountServices.getAccountsOwnedBy(loggedOnUser)
+                            .Where(acc => acc.ID == account.ID).Count() != 0)
+                    return new SolidColorBrush(Color.FromArgb(0xff, 0x22, 0xcc, 0x55));
 
-            // Active
-            else if (AccountServices.getAccountsRentedBy(loggedOnUser, true)
-                        .Where(acc => acc.ID == account.ID).Count() != 0)
-                return new SolidColorBrush(Color.FromArgb(0xff, 0x88, 0xaa, 0x22));
+                // Active
+                else if (AccountServices.getAccountsRentedBy(loggedOnUser, true)
+                            .Where(acc => acc.ID == account.ID).Count() != 0)
+                    return new SolidColorBrush(Color.FromArgb(0xff, 0x88, 0xaa, 0x22));
 
-            // Booked
-            else if (AccountServices.getAccountsRentedBy(loggedOnUser, false)
-                        .Where(acc => acc.ID == account.ID).Count() != 0)
-                return new SolidColorBrush(Color.FromArgb(0xff, 0x11, 0x33, 0x99));
-
+                // Booked
+                else if (AccountServices.getAccountsRentedBy(loggedOnUser, false)
+                            .Where(acc => acc.ID == account.ID).Count() != 0)
+                    return new SolidColorBrush(Color.FromArgb(0xff, 0x11, 0x33, 0x99));
+            }
             return new SolidColorBrush(Color.FromArgb(0x00, 0x00, 0x00, 0x00));
         }
 
